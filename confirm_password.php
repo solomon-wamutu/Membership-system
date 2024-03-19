e00be0ab85b0dee278db85ffae37e43516e3b824
solomon wamuutu <solomonwamutu16@gmail.com>
2024-03-19 21:55:10 +0300

    header

--- confirm_password.php
+++ confirm_password.php
@@ -47,75 +47,97 @@ if (isset($_POST['confirm_reset_password'])) {
 // $res = $stmt->get_result();
 // while ($auth = $res->fetch_object()) {
 ?>
-    <!DOCTYPE html>
-    <html>
-    <?php //include("dist/_partials/head.php"); ?>
+<!DOCTYPE html>
 
-    <body class="hold-transition login-page">
-        <div class="login-box">
-            <div class="login-logo">
-                <p><?php //echo $auth->sys_name; ?>  <?php ///echo $auth->sys_tagline; ?></p>
-            </div>
-            <!-- /.login-logo -->
-            <div class="card">
-                <div class="card-body login-card-body">
-                    <?php
-                    $email  = $_SESSION['email'];
-                    $ret = "SELECT * FROM  `users`  WHERE email = '$email'";
-                    $stmt = $conn->prepare($ret);
-                    $stmt->execute(); //ok
-                    $res = $stmt->get_result();
-                    while ($row = $res->fetch_object()) {
-                    ?>
-                        <p class="login-box-msg"><?php echo $row->email; ?> Please Enter And Confirm Your Password</p>
-                    <?php
-                    } ?>
-                    <form method="POST">
-                        <div class="input-group mb-3">
-                            <input type="password" required name="new_password" class="form-control" placeholder="New Password">
-                            <div class="input-group-append">
-                                <div class="input-group-text">
-                                    <span class="fas fa-lock"></span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="input-group mb-3">
-                            <input type="password" required name="confirm_password" class="form-control" placeholder="Confirm Password">
-                            <div class="input-group-append">
-                                <div class="input-group-text">
-                                    <span class="fas fa-lock"></span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12">
-                                <button type="submit" name="confirm_reset_password" class="btn btn-success btn-block">Reset Password</button>
-                            </div>
-                            <!-- /.col -->
-                        </div>
-                    </form>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Membership Management - codeastro.com</title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-                    <p class="mt-3 mb-1">
-                        <a href="index.php">Login</a>
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <!-- icheck bootstrap -->
+    <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="dist/css/adminlte.min.css">
+    <!-- Google Font: Source Sans Pro -->
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+</head>
+<html>
+<?php //include("dist/_partials/head.php"); 
+?>
 
-                    </p>
-
-                </div>
-                <!-- /.login-card-body -->
-            </div>
+<body class="hold-transition login-page">
+    <div class="login-box">
+        <div class="login-logo">
+            <p><?php //echo $auth->sys_name; 
+                ?> <?php ///echo $auth->sys_tagline; 
+                                                ?></p>
         </div>
-        <!-- /.login-box -->
+        <!-- /.login-logo -->
+        <div class="card">
+            <div class="card-body login-card-body">
+                <?php
+                $email  = $_SESSION['email'];
+                $ret = "SELECT * FROM  `users`  WHERE email = '$email'";
+                $stmt = $conn->prepare($ret);
+                $stmt->execute(); //ok
+                $res = $stmt->get_result();
+                while ($row = $res->fetch_object()) {
+                ?>
+                    <p class="login-box-msg"><?php echo $row->email; ?> Please Enter And Confirm Your Password</p>
+                <?php
+                } ?>
+                <form method="POST">
+                    <div class="input-group mb-3">
+                        <input type="password" required name="new_password" class="form-control" placeholder="New Password">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-lock"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="input-group mb-3">
+                        <input type="password" required name="confirm_password" class="form-control" placeholder="Confirm Password">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-lock"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12">
+                            <button type="submit" name="confirm_reset_password" class="btn btn-success btn-block">Reset Password</button>
+                        </div>
+                        <!-- /.col -->
+                    </div>
+                </form>
 
-        <!-- jQuery -->
-        <script src="plugins/jquery/jquery.min.js"></script>
-        <!-- Bootstrap 4 -->
-        <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-        <!-- AdminLTE App -->
-        <script src="dist/js/adminlte.min.js"></script>
+                <p class="mt-3 mb-1">
+                    <a href="index.php">Login</a>
 
-    </body>
+                </p>
 
-    </html>
+            </div>
+            <!-- /.login-card-body -->
+        </div>
+    </div>
+    <!-- /.login-box -->
+
+    <!-- jQuery -->
+    <script src="plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap 4 -->
+    <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="dist/js/adminlte.min.js"></script>
+
+</body>
+
+</html>
 <?php
 
 // } 
